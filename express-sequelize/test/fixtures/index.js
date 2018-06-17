f4ef4639ce8bb811f1e9cbfb653f66a6cfb93cb5
aguestuser <aguestuser@riseup.net>
2018-06-17 18:47:08 -0400

    [es] test bulk CRUD ops on User model

---
+++ express-sequelize/test/fixtures/index.js
@@ -0,0 +1,16 @@
+export const userAttrs = {
+  username: "pynchon",
+  email: "pynchon@riseup.net",
+}
+
+export const usersAttrs = [
+  userAttrs,
+  {
+    username: "egoldman",
+    email: "egoldman@riseup.net",
+  },
+  {
+    username: "eliot",
+    email: "eliot@evilcorp.biz",
+  }
+]
--- express-sequelize/test/models/user.spec.js
+++ express-sequelize/test/models/user.spec.js
@@ -1,10 +1,10 @@
 import {expect} from "chai"
-import {pick, keys, omit} from 'lodash'
-import UserService from "../../src/services/userService"
-import db, {sequelize} from "../../src/models"
+import {pick, keys, map, slice, times} from 'lodash'
+import db, {sequelize, Sequelize} from "../../src/models"
+import {userAttrs, usersAttrs} from "../fixtures"
+const {Op} = Sequelize
 
 describe("UserService", () => {
-  const userAttrs = { username: "pynchon", email: "pynchon@risup.net" }
   let userCount
   before(async () => {
     await sequelize.authenticate()
@@ -41,7 +41,7 @@ describe("UserService", () => {
       it("updates a user via a static method", async () => {
         await db.User.update(
           { username: "oedipa" },
-          { where: {id: user.id} },
+          { where: { id: user.id } },
         )
         const newUsername = await db.User
           .findOne({where: {id: user.id}})
@@ -64,6 +64,66 @@ describe("UserService", () => {
         await db.User.destroy({ where: { id: user.id}})
         expect(await db.User.count()).to.eql(userCount - 1)
       })
+    })
+  })
+
+  describe("bulk CRUD operations", () => {
+    let users
+    beforeEach(async () => users = await db.User.bulkCreate(usersAttrs))
+    afterEach(async () => await db.User.destroy({ where: {}}))
+
+    it("creates many users", async () => {
+      expect(await db.User.count()).to.eql(userCount + 3)
+    })
+
+    it("retrieves all users", async () => {
+      const foundUsers = await db.User.findAll()
+      expect(
+        map(foundUsers, u => pick(u.dataValues, keys(userAttrs)))
+      ).to.eql(usersAttrs)
+    })
+
+    it("retrieves all users matching search criteria", async () => {
+      const foundUsers = await db.User.findAll({
+        where: { email: { [Op.like]: "%riseup%" } }
+      })
+      expect(
+        map(foundUsers, u => pick(u.dataValues, keys(userAttrs)))
+      ).to.eql(slice(usersAttrs,0,2))
+    })
+
+    it("updates all users", async () => {
+      await db.User.update(
+        { email: "foo@bar.com" },
+        { where: {} }
+      )
+      expect(map(await db.User.findAll(), u => u.email))
+        .to.eql(times(3, () => "foo@bar.com"))
+    })
+
+    it("updates many users", async () => {
+      const updatedUsers = await db.User.findAll({
+        where: { email: { [Op.like]: "%riseup%" } }
+      }).then(users => Promise.all(
+          map(users, u => u.update({email: u.email.replace("riseup", "microsoft")}))
+        )
+      )
+
+      expect(map(updatedUsers, u => u.email)).to.eql([
+        "pynchon@microsoft.net",
+        "egoldman@microsoft.net"
+      ])
+
+      expect(await db.User.count({
+        where: { email: { [Op.like]: "%microsoft%" }}
+      })).to.eql(2)
+    })
+
+    it("deletes many users", async () => {
+      await db.User.destroy({
+        where: { email: { [Op.like]: "%riseup%" } }
+      })
+      expect(await db.User.count()).to.eql(1)
     })
   })
 })
